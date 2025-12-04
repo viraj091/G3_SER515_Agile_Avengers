271a6d73c5e7f5bedd67260fa5c7a7b1042fb07a
srevank2 <srevank2@asu.edu>
2025-12-04 16:41:07 -0700

    Added service level guard for story status updates

--- src/main/java/com/scrumsim/service/DefaultAuthService.java
+++ src/main/java/com/scrumsim/service/DefaultAuthService.java
@@ -12,6 +12,8 @@ public class DefaultAuthService implements AuthService {
 
     private final Map<String, UserRole> roleDatabase;
 
+    private final Map<String, String> nameDatabase;
+
     public DefaultAuthService() {
 
         passwordDatabase = new HashMap<>();
@@ -19,13 +21,34 @@ public class DefaultAuthService implements AuthService {
         passwordDatabase.put("po", "po123");
         passwordDatabase.put("dev", "dev123");
         passwordDatabase.put("qa", "qa123");
+        passwordDatabase.put("viraj rathor", "viraj123");
+        passwordDatabase.put("gunjan purohit", "gunjan123");
+        passwordDatabase.put("sairaj dalvi", "sairaj123");
+        passwordDatabase.put("pranav irlapale", "pranav123");
+        passwordDatabase.put("shreyas revankar", "shreyas123");
 
- 
+
         roleDatabase = new HashMap<>();
         roleDatabase.put("sm", UserRole.SCRUM_MASTER);
         roleDatabase.put("po", UserRole.PRODUCT_OWNER);
         roleDatabase.put("dev", UserRole.DEVELOPER);
-        roleDatabase.put("qa", UserRole.DEVELOPER); 
+        roleDatabase.put("qa", UserRole.DEVELOPER);
+        roleDatabase.put("viraj rathor", UserRole.DEVELOPER);
+        roleDatabase.put("gunjan purohit", UserRole.DEVELOPER);
+        roleDatabase.put("sairaj dalvi", UserRole.DEVELOPER);
+        roleDatabase.put("pranav irlapale", UserRole.DEVELOPER);
+        roleDatabase.put("shreyas revankar", UserRole.DEVELOPER);
+
+        nameDatabase = new HashMap<>();
+        nameDatabase.put("sm", "sm");
+        nameDatabase.put("po", "po");
+        nameDatabase.put("dev", "dev");
+        nameDatabase.put("qa", "qa");
+        nameDatabase.put("viraj rathor", "Viraj Rathor");
+        nameDatabase.put("gunjan purohit", "Gunjan Purohit");
+        nameDatabase.put("sairaj dalvi", "Sairaj Dalvi");
+        nameDatabase.put("pranav irlapale", "Pranav Irlapale");
+        nameDatabase.put("shreyas revankar", "Shreyas Revankar");
     }
 
     @Override
@@ -46,8 +69,9 @@ public class DefaultAuthService implements AuthService {
         }
 
         UserRole role = roleDatabase.get(normalizedUsername);
+        String properName = nameDatabase.get(normalizedUsername);
 
-        return new User(normalizedUsername, role);
+        return new User(properName, role);
     }
 
    
